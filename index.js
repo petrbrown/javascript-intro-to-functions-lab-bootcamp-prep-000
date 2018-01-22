97b2f11e027783ae89f7ae43209aa53de76dea07
petr brown <petrbrown@gmail.com>
2018-01-22 11:35:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,11 +18,9 @@ function sayHitoGrandma(string)
 {
   var uppercase = "string.toUpperCase()"
   var lowercase = "string.toLowerCase()"
-  if (string === lowercase) {
+  if (string === ${lowercase}) {
     return ("I can\'t hear you")
   } else if (string === uppercase) {
     return ('')
   }
-  if (sayHitoGrandma(string.toUpperCase()===)
-  return sayHitoGrandma(string)
 }
