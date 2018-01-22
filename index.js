3fb2bfc3722c6846cd6667899891f8d71991aadd
petr brown <petrbrown@gmail.com>
2018-01-22 12:29:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function shout(string)
 {
-  return (string.toUpperCase())
+  return string.toUpperCase()
 }
 function whisper(string)
 {
-  return (string.toLowerCase())
+  return string.toLowerCase()
 }
 function logWhisper(string)
 {
