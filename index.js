cc48fef69b53da435ec54ba77e4238b8abab96af
petr brown <petrbrown@gmail.com>
2018-01-22 11:10:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string)
+{
+  return (string.toUpperCase())
+}
+function whisper(string)
+{
+  return (string.toLowerCase())
+}
+function logWhisper(string)
+{
+  console.log(string.toLowerCase())
+}
+function logShout(string)
+{
+  console.log(string.toUpperCase())
+}
+function sayHitoGrandma(string, function())
+{
+  whisper()
+  return sayHitoGrandma(string
+}
